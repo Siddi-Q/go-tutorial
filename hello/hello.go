4de82c49379830888b71eadf202252597c1801cc
Saddique Shafi <saddiqueashafi@gmail.com>
2020-09-15 12:32:29 -0400

    Added functionality that returns greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -13,8 +13,11 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Gladys")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	message, err := greetings.Hellos(names)
 
 	// If an error was returned, print it to the console and exit the program.
 	if err != nil {
